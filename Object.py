a87b673dca977dd18a6974bb70835dbf8714bf15
Rubenwosten <rubenwosten@outlook.com>
2024-11-28 11:33:07 +0100

    Object class

---
+++ Object.py
@@ -0,0 +1,9 @@
+class Object:
+    def __init__(self,instancetoken, sampletoken, translation, rotation, size, catogory ):
+        self.objecttoken = instancetoken
+        self.sampletoken = sampletoken
+        self.translation = translation
+        self.rotation = rotation
+        self.size = size
+        self.catogory = catogory
+        
