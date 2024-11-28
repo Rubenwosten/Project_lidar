eeb263906fea2ad4c2e809b012b560de841e98be
Rubenwosten <rubenwosten@outlook.com>
2024-11-28 11:35:44 +0100

    imports

--- Object.py
+++ Object.py
@@ -1,3 +1,23 @@
+from nuscenes.nuscenes import NuScenes 
+import matplotlib.pyplot as plt
+import numpy as np
+
+from nuscenes.map_expansion.map_api import NuScenesMap
+from nuscenes.map_expansion import arcline_path_utils
+from nuscenes.map_expansion.bitmap import BitMap
+from nuscenes.eval.prediction.splits import get_prediction_challenge_split
+from nuscenes.prediction import PredictHelper
+
+from nuscenes.prediction.input_representation.static_layers import StaticLayerRasterizer
+from nuscenes.prediction.input_representation.agents import AgentBoxesWithFadedHistory
+from nuscenes.prediction.input_representation.interface import InputRepresentation
+from nuscenes.prediction.input_representation.combinators import Rasterizer
+
+from nuscenes.prediction.models.backbone import ResNetBackbone
+from nuscenes.prediction.models.mtp import MTP
+from nuscenes.prediction.models.covernet import CoverNet
+import torch
+
 class Object:
     def __init__(self,instancetoken, sampletoken, translation, rotation, size, catogory ):
         self.objecttoken = instancetoken
@@ -7,3 +27,4 @@ class Object:
         self.size = size
         self.catogory = catogory
         
+    def voorspelling(self):
