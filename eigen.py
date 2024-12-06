5605b8eaeb6f4e707ce86fc40bec205ed9147228
Rubenwosten <rubenwosten@outlook.com>
2024-12-06 10:02:23 +0100

    kutmessage

--- eigen.py
+++ eigen.py
@@ -22,7 +22,7 @@ import torch
 
 
 nusc = NuScenes(version='v1.0-mini', verbose=False)
-
+dataroot='C:/Users/Ruben/OneDrive/Bureaublad/data/sets/nuscenes'
 nusc_map = NuScenesMap(dataroot='C:/Users/Ruben/OneDrive/Bureaublad/data/sets/nuscenes', map_name='singapore-onenorth')
 bitmap = BitMap(nusc_map.dataroot, nusc_map.map_name, 'basemap')
 
@@ -80,7 +80,8 @@ def Anns(sample):
 samples = samples_scene(first, last)
 info = nusc.get('sample', samples[0])
 anns = info['anns']
-info = nusc.get('sample_annotation', anns[1])
+a = anns[0]
+info = nusc.get('sample_annotation', a)
 print (info)
 
 lidar_info =nusc.calibrated_sensor[5]
@@ -98,3 +99,60 @@ for i in range(len(scan)):
         phi = phi/180
 
 
+info =  nusc.get('sample', first)
+info_2 = nusc.get('sample_data',info['data']['LIDAR_TOP'])
+file = os.path.join(dataroot, info_2['filename'])
+print(file)
+print(type(file))
+
+som = 0
+lidar_punt=0
+            
+with open(file, "rb") as f:
+    number = f.read(4)
+    print (number)
+    while number != b"":
+        quo, rem = divmod(som,5)
+        if rem == 0:
+            x = np.frombuffer(number, dtype=np.float32)
+        elif rem ==1:
+            y = np.frombuffer(number, dtype=np.float32)
+            lidar_punt += 1
+        number = f.read(4)
+        print (number)
+        som +=1
+print (som)
+
+
+
+R = 10
+dt = 10
+dt_rad = dt/180
+x= 0
+reso = 1
+theta = 0
+riskcones = np.empty(360/dt)
+total_risk = 0
+for i in range(360/dt):
+    while x < R:
+        if (theta>= -0.5*np.pi and theta<0.5*np.pi):
+            a= 1
+        else: a= -1
+        y1 = x*np.tan(theta)
+        y2 = x*np.tan(theta)
+        if y2 < y1:
+            ymin = y2
+            ymax = y1
+        else:
+            ymin = y1
+            ymax = y2
+        while ymin < ymax:
+            risk = risk.cell(a*x, ymin)
+            riskcones[i] += risk
+            total_risk += risk
+
+for i in range(len(riskcones)):
+    
+
+
+    
